5424d93b7b239a56e50d2975f1e81ed9de5d9d98
V011dd <void5470@gmail.com>
2026-05-16 13:10:39 +0800

    skip migration test nginx

--- start.sh
+++ start.sh
@@ -2,17 +2,11 @@
 echo "=== Starting Container ==="
 echo "PORT: $PORT"
 
-echo "=== Running Migration ==="
-php artisan migrate --force
+echo "=== Updating nginx port ==="
+sed -i "s/listen 8000/listen $PORT/" /etc/nginx/sites-available/default
 
 echo "=== Starting PHP-FPM ==="
 service php8.3-fpm start
-echo "PHP-FPM status: $?"
-
-echo "=== Updating nginx port ==="
-sed -i "s/listen 8000/listen $PORT/" /etc/nginx/sites-available/default
-cat /etc/nginx/sites-available/default
 
 echo "=== Starting nginx ==="
-nginx -t
 nginx -g 'daemon off;'
