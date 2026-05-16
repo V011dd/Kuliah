341576c6bc38370892007b3d5b14b67fd2c07999
V011dd <void5470@gmail.com>
2026-05-16 13:04:35 +0800

    fix nginx dynamic port

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 php artisan migrate --force
+
+# Ganti port di nginx config sesuai PORT dari Railway
+sed -i "s/listen 8000/listen $PORT/" /etc/nginx/sites-available/default
+
 service php8.3-fpm start
 nginx -g 'daemon off;'
