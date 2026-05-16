ec3072dbe1c75f3fb15d11f54b89636c7b554aaa
V011dd <void5470@gmail.com>
2026-05-16 13:16:41 +0800

    fix php-fpm tcp socket

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@
 echo "=== Starting Container ==="
 echo "PORT: $PORT"
 
+echo "=== Starting PHP-FPM ==="
+php-fpm8.3 -D
+
 echo "=== Updating nginx port ==="
 sed -i "s/listen 8000/listen $PORT/" /etc/nginx/sites-available/default
-
-echo "=== Starting PHP-FPM ==="
-service php8.3-fpm start
 
 echo "=== Starting nginx ==="
 nginx -g 'daemon off;'
